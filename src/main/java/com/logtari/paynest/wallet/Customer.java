34a2b4502c7582a805d743bd4fe32c8a7ab3fe10
morrta <mohamedmortadha.logtari@gmail.com>
2026-09-01 20:52:33 +0100

    feat: Introduce CustomerTest and Customer: - Customer ID should not be null - Customer Name should not be Blank Added apache common lang 3.20 dependency

---
+++ src/main/java/com/logtari/paynest/wallet/Customer.java
@@ -0,0 +1,22 @@
+package com.logtari.paynest.wallet;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Objects;
+
+@Getter
+@EqualsAndHashCode
+public class Customer {
+    private final CustomerId customerId;
+    private final String name;
+
+    public Customer(CustomerId customerId, String name) {
+        this.customerId = Objects.requireNonNull(customerId, "Customer ID must not be null");
+        if(StringUtils.isBlank(name)){
+            throw new IllegalArgumentException("Customer name must not be Blank");
+        }
+        this.name = name;
+    }
+}
---
+++ src/test/java/com/logtari/paynest/wallet/CustomerTest.java
@@ -0,0 +1,40 @@
+package com.logtari.paynest.wallet;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class CustomerTest {
+    @Test
+    void shouldCreateCustomer() {
+        CustomerId id = CustomerId.generate();
+
+        Customer customer = new Customer(id, "Mohamed");
+
+        assertThat(customer.getCustomerId()).isEqualTo(id);
+        assertThat(customer.getName()).isEqualTo("Mohamed");
+    }
+
+    @Test
+    void shouldRejectBlankName() {
+        assertThatThrownBy(() ->
+                new Customer(
+                        CustomerId.generate(),
+                        " "
+                )
+        )
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void shouldRejectNullName() {
+        assertThatThrownBy(() ->
+                new Customer(
+                        CustomerId.generate(),
+                        null
+                )
+        )
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
